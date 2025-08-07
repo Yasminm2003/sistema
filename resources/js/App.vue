dfd3d1e74d933290bddb4ec085735da551a45227
Yasmin Moreira <yasminmoreirac2003@gmail.com>
2025-08-07 09:59:11 -0300

    adicionar arquivo

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,34 @@
 <template>
-  <Departamento />
+  <div class="container mt-4">
+    <h2 class="mb-4 text-center">Sistema de Cadastro</h2>
+    <div class="row">
+      <div class="col-md-6">
+        <Departamento @salvar="handleDepartamentoSalvo" />
+      </div>
+      <div class="col-md-6">
+        <Item @salvar="handleItemSalvo" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import Departamento from './components/Departamento.vue'
+import Item from './components/Item.vue'
+
+const handleDepartamentoSalvo = (dados) => {
+  console.log('Departamento salvo:', dados)
+  // Aqui você pode atualizar listas ou mostrar alertas, se precisar
+}
+
+const handleItemSalvo = (dados) => {
+  console.log('Item salvo:', dados)
+  // Similar ao departamento, você pode reagir ao evento de salvamento
+}
 </script>
+
+<style scoped>
+.container {
+  max-width: 1200px;
+}
+</style>
